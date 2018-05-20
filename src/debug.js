9d3e73b767381ee0120b1a503df8fa156a95577b
Rayzr522 <rayzr522@gmail.com>
2018-05-19 22:16:37 -0400

    Cleaned up starter script

--- src/debug.js
+++ src/debug.js
@@ -5,9 +5,9 @@ const PREFIX = '[ZStream]';
 
 const log = (color, message) => console.log(`${color(PREFIX)} ${message}`);
 
-const info = log.bind(null, [chalk.green]);
+const info = log.bind(null, chalk.green);
 
-const error = log.bind(null, [chalk.red]);
+const error = log.bind(null, chalk.red);
 
 const variable = text => chalk.yellow(text);
 
